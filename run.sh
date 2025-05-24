079d7734f455f47de199d85ac04dca0d2ed7058d
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-24 19:49:10 +0700

    fix npm

--- run.sh
+++ run.sh
@@ -111,7 +111,7 @@ print_green "Configuration files have been checked."
 
 print_yellow "Checking dependencies..."
 cd "$MODULES_DIR"
-npm install --no-audit --no-fund --prefer-offline --force user-agents axios meo-forkcy-colors https-proxy-agent socks-proxy-agent @google/genai
+npm install --no-audit --no-fund --prefer-offline --force 
 cd - > /dev/null
 print_green "Dependencies installation completed!"
 
